3437115e23fb473c3c63a65a6b62c53c5fb52a65
Vepchansky <troll75677@gmail.com>
2024-02-05 23:57:59 +0300

    Create method to initialized array Train's

--- lab_3/task1.java
+++ lab_3/task1.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 import java.util.Random;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.ArrayList;
 
 public class task1 {
+    public static List<Train> setTrains(List<Train> testTrains) {
+       int[] testSeats = {100, 50, 30, 20};
+       testTrains.add(new Train("Казань", 7777, LocalTime.of(1, 2, 3), testSeats));
+       return testTrains;
+    }
+
     public static void main(String[] args){
+
+        LocalTime testTime =  LocalTime.of(1, 2, 3);
+        int[] testSeats = {100, 50, 30, 20};
 
         Train t1 = new Train();
         Train[] tm = {
             new Train(),
-            new Train(),
-            new Train(),
+            new Train("Москва", 666, testTime),
+            new Train("Казань", 7777, testTime, testSeats),
         };
 
         // Проверка методов get и set
@@ -21,6 +32,12 @@ public class task1 {
 
         System.out.println(t1);
         System.out.println(tm[1]);
+        System.out.println(tm[2]);
+
+        // Проверка заполнения массива
+
+        List<Train> trains = task1.setTrains(new ArrayList<Train>());
+        System.out.println(trains);
 
 
     }
@@ -46,6 +63,8 @@ class Train {
         this.luxurySeats = 0;
     }
 
+    // Определяем конструкторы
+
     Train() {
         this.setDestPoint("Голутвин");
         this.setTrainNum(6432);
@@ -55,6 +74,31 @@ class Train {
         this.setReservedSeats(500);
         this.setLuxurySeats(100);
         
+    }
+
+    Train(String destPoint) {
+        this.setDestPoint(destPoint);
+    }
+
+    Train(String destPoint, int trainNum) {
+        this.setDestPoint(destPoint);
+        this.setTrainNum(trainNum);
+    }
+
+    Train(String destPoint, int trainNum, LocalTime depTime) {
+        this.setDestPoint(destPoint);
+        this.setTrainNum(trainNum);
+        this.setDepTime(depTime);
+    }
+
+    Train(String destPoint, int trainNum, LocalTime depTime, int[] seats) {
+        this.setDestPoint(destPoint);
+        this.setTrainNum(trainNum);
+        this.setDepTime(depTime);
+        this.setTotalSeats(seats[0]);
+        this.setCompSeats(seats[1]);
+        this.setReservedSeats(seats[2]);
+        this.setLuxurySeats(seats[3]);
     }
 
     // Определяем геттеры
@@ -73,6 +117,9 @@ class Train {
     public void setTrainNum(int tNum) { this.trainNumber = tNum; }
     public void setDepTime(int h, int m, int s) {
         this.departureTime = LocalTime.of(h, m, s); 
+    }
+    public void setDepTime(LocalTime depTime) {
+        this.departureTime = depTime; 
     }
     public void setTotalSeats(int tSeats) { this.totalSeats = tSeats; }
     public void setCompSeats(int cSeats) { this.compartmentSeats = cSeats; }
